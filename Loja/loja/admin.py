a9ac78130faf2cfbe9ae9b6f7c617a192005bd54
Ranielly <ranielly.brito@escolar.ifrn.edu.br>
2026-05-08 13:47:33 +0000

    mensagem

---
+++ Loja/loja/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import * #imporata nossos models
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+    fields = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    search_fields = ('Produto',)
+class ProdutoAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    fields = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    search_fields = ('Produto',)
+admin.site.register(Fabricante,FabricanteAdmin) #adiciona a interface do admim
+admin.site.register(Categoria) #adiciona a interface do admim
+admin.site.register(Produto, ProdutoAdmin) #adiciona a interface do admim
---
+++ Loja/loja/views/ProdutoView.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+def list_produto_view(request, id=None):
+        if id is None:
+                return HttpResponse('<h1>Nenhum id foi informado</h1>')
+        return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+
+urlpatterns = [
+path("", list_produto_view, name='produtos'),
+path("<int:id>", list_produto_view, name='produto'),
+]
